a4ef51a2b681ad3d7e68946112def20c8b660fc3
Vladimir Andreev <105918724+VladimirAndreev1982@users.noreply.github.com>
2022-07-04 10:33:14 +0300

    Change_1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 100; // начальный счёт
-        int b = 1100; // сумма пополнения
-        int c = 100; // один бонус за каждые 100 руб.
-        int d = 1000; // расчёт бонусов если клиент пополняет счёт более чем на 1000 руб.
+        int deposit_start = 100; // начальный счёт
+        int deposit_add = 1100; // сумма пополнения
+        int bonus_cost = 100; // один бонус за каждые 100 руб.
+        int bonus_condition = 1000; // расчёт бонусов если клиент пополняет счёт более чем на 1000 руб.
 
-        int x;
-        int s;
+        int bonus_quantity; // кол-во бонусов
+        int bonus_all; // итоговая сумма с бонусами
 
-        if (b > d) {
-            x = b / c; // кол-во бонусов
-            s = a + b + x; // итоговая сумма с бонусами
+        if (deposit_add > bonus_condition) {
+            bonus_quantity = deposit_add / bonus_cost; // кол-во бонусов
+            bonus_all = deposit_start + deposit_add + bonus_quantity; // итоговая сумма с бонусами
         } else {
-            x = 0; // кол-во бонусов
-            s = a + b + x; // итоговая сумма с бонусами
+            bonus_quantity = 0; // кол-во бонусов
+            bonus_all = deposit_start + deposit_add + bonus_quantity; // итоговая сумма с бонусами
         }
-        System.out.println("Количество бонусных рублей: " + x);
-        System.out.println("Итоговый счёт: " + s);
+        System.out.println("Количество бонусных рублей: " + bonus_quantity);
+        System.out.println("Итоговый счёт: " + bonus_all);
     }
 }
