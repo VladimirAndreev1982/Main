45ccd3235af2518176393c14bf4d875d449b0e3e
Vladimir Andreev <105918724+VladimirAndreev1982@users.noreply.github.com>
2022-07-05 09:25:04 +0300

    Changes_2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int deposit_start = 100; // начальный счёт
-        int deposit_add = 1100; // сумма пополнения
-        int bonus_cost = 100; // один бонус за каждые 100 руб.
-        int bonus_condition = 1000; // расчёт бонусов если клиент пополняет счёт более чем на 1000 руб.
+        int depositStart = 100; // начальный счёт
+        int depositAdd = 1100; // сумма пополнения
+        int bonusCost = 100; // один бонус за каждые 100 руб.
+        int bonusCondition = 1000; // расчёт бонусов если клиент пополняет счёт более чем на 1000 руб.
 
-        int bonus_quantity; // кол-во бонусов
-        int bonus_all; // итоговая сумма с бонусами
+        int bonusQuantity; // кол-во бонусов
+        int bonusAll; // итоговая сумма с бонусами
 
-        if (deposit_add > bonus_condition) {
-            bonus_quantity = deposit_add / bonus_cost; // кол-во бонусов
-            bonus_all = deposit_start + deposit_add + bonus_quantity; // итоговая сумма с бонусами
+        if (deposit_Add > bonusCondition) {
+            bonusQuantity = depositAdd / bonusCost; // кол-во бонусов
+            bonusAll = depositStart + depositAdd + bonusQuantity; // итоговая сумма с бонусами
         } else {
-            bonus_quantity = 0; // кол-во бонусов
-            bonus_all = deposit_start + deposit_add + bonus_quantity; // итоговая сумма с бонусами
+            bonusQuantity = 0; // кол-во бонусов
+            bonusAll = depositStart + depositAdd + bonusQuantity; // итоговая сумма с бонусами
         }
-        System.out.println("Количество бонусных рублей: " + bonus_quantity);
-        System.out.println("Итоговый счёт: " + bonus_all);
+        System.out.println("Количество бонусных рублей: " + bonusQuantity);
+        System.out.println("Итоговый счёт: " + bonusAll);
     }
 }
